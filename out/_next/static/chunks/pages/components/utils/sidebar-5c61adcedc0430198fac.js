9176e1e02f97cc5717263a415879df3faefdf75f
FENDSBOOK <machmudeffendi01@gmail.com>
2021-06-05 20:57:12 +0700

    add out/

---
+++ out/_next/static/chunks/pages/components/utils/sidebar-5c61adcedc0430198fac.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[952],{4282:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/utils/sidebar",function(){return u(1423)}])}},function(n){n.O(0,[774,888,179],(function(){return _=4282,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
