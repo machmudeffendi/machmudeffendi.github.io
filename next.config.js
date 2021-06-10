153636a912f17340e26c4d9203bf8899f703873c
machmudfends <machmudeffendi@gmail.com>
2021-06-10 17:12:16 +0700

    fix asset #2

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? 'https://machmudeffendi.github.io' : '',
+  assetPrefix: isProd ? 'https://machmudeffendi.github.io/awesome' : '',
 }
