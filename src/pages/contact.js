afb661dfb79b76c11d9700d7da2d0486c45ad548
machmudfends <machmudeffendi@gmail.com>
2021-06-11 08:04:04 +0700

    INITIAL COMMIT MIGRATE GATSBY

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact(){
+  return (
+    <div className="header">
+      <span className="sub-title">Get In Touch</span>
+      <h2 className="title">Contact</h2>
+    </div>
+  )
+}
---
+++ src/pages/work.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Work(){
+  return (
+    <div className="header">
+      <span className="sub-title">My Work</span>
+      <h2 className="title">Recent Work</h2>
+    </div>
+  )
+}
