096677da810d0e60657bcc9b31b147d26aeff2b0
Machmud Effendi <machmud.effendi@syngenta.com>
2025-02-27 00:40:17 +0700

    feat: layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,98 +2,74 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className="flex flex-col h-screen">
+      <nav className="flex flex-row border border-[#2C334B] h-[55px] align-middle">
+        <span className="border-r border-[#2C334B] px-[20px] content-center">
+          machmud-effendi
+        </span>
+        <a role="button" className="border-r border-[#2C334B] px-[20px] content-center hover:border-b-4 hover:border-b-[#9290C3] hover:text-white">
+          _home
+        </a>
+        <a role="button" className="border-r border-[#2C334B] px-[20px] content-center hover:border-b-4 hover:border-b-[#9290C3] hover:text-white">
+          _about-me
+        </a>
+        <a role="button" className="border-r border-[#2C334B] px-[20px] content-center hover:border-b-4 hover:border-b-[#9290C3] hover:text-white">
+          _blogs
+        </a>
+        <a role="button" className="border-r border-[#2C334B] px-[20px] content-center hover:border-b-4 hover:border-b-[#9290C3] hover:text-white">
+          _projects
+        </a>
+        <a role="button" className="ms-auto border-l border-[#2C334B] px-[20px] content-center hover:border-4 hover:border-[#9290C3] hover:text-white">
+          _contact-me
+        </a>
+      </nav>
+      <main className="flex-grow border-l border-r border-[#2C334B] p-3">
+        Content
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <footer className="flex flex-row border border-[#2C334B] h-[45px] align-middle">
+        <span className="border-r border-[#2C334B] px-[20px] content-center">
+          find-me :
+        </span>
+        <a role="button" className="border-r border-[#2C334B] px-[20px] content-center hover:text-white">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            className="dark:invert"
+            src="/twitter.svg"
+            alt="Twitter logo"
+            width={24}
+            height={24}
+            priority
           />
-          Learn
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        <a role="button" className="border-r border-[#2C334B] px-[20px] content-center hover:text-white">
+        <Image
+            className="dark:invert"
+            src="/instagram.svg"
+            alt="Instagram logo"
+            width={24}
+            height={24}
+            priority
           />
-          Examples
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+        <a role="button" className="border-r border-[#2C334B] px-[20px] content-center hover:text-white">
+        <Image
+            className="dark:invert"
+            src="/linkedin.svg"
+            alt="Linkedin logo"
+            width={24}
+            height={24}
+            priority
           />
-          Go to nextjs.org →
+        </a>
+        <a role="button" className="flex flex-row ms-auto border-l border-[#2C334B] px-[20px] content-center hover:text-white">
+          <span className="content-center me-2">@machmudeffendi</span> 
+          <Image
+            className="dark:invert"
+            src="/github.svg"
+            alt="Github logo"
+            width={24}
+            height={24}
+            priority
+          />
         </a>
       </footer>
     </div>
