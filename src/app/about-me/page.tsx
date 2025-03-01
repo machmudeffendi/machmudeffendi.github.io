f7669ddc60f10c2fb2df4c9c83e6837917a21258
Machmud Effendi <machmud.effendi@syngenta.com>
2025-03-01 13:41:41 +0700

    fix: layout about me

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -4,7 +4,7 @@ import { EDUCATION, EXPEREINCE } from "@/utility/constant"
 
 export default function AboutMe(){
   return (
-    <div className="flex flex-col gap-[20px]">
+    <div className="flex flex-col gap-[20px] lg:px-12">
       <Box title="Executive Summary">
         <p className="text-white">A highly skilled Software Engineer with 5 years of experience in designing, developing, and deploying scalable software solutions. Proficient in multiple programming languages and frameworks, with a strong background in software architecture, system optimization and problem-solving. Adept at collaborating with cross-functional teams to deliver high-quality products that meet business objectives. Passionate about continuous learning and leveraging emerging technologies to drive innovation and efficiency in software development.</p>
       </Box>
