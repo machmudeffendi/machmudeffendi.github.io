a732787138319a84e53b0127c9cd2c5740748e7d
Machmud Effendi <machmud.effendi@syngenta.com>
2025-03-01 12:41:16 +0700

    feat: responsive home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,34 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="grid grid-cols-2 h-full">
-    <div className="flex flex-col justify-center gap-[50px]">
+    <div className="grid grid-cols-1 md:grid-cols-2 h-full items-center gap-1 md:gap-8 px-1 md:px-12">
+    {/* Left Section - Text Content */}
+    <div className="flex flex-col justify-center gap-6 text-center md:text-left">
       <div className="flex flex-col leading-none">
-        <span className="text-white text-[20px]">Hi There, I am</span>
-        <span className="text-white text-[64px] font-bold">Machmud Effendi</span>
-        <span className="text-[#1F37C0] text-[32px]">{'>'} Software Engineer</span>
+        <span className="text-white text-lg md:text-xl">Hi There, I am</span>
+        <span className="text-white text-4xl md:text-6xl font-bold">Machmud Effendi</span>
+        <span className="text-[#1F37C0] text-2xl md:text-4xl">{'>'} Software Engineer</span>
       </div>
       <div className="flex flex-col leading-tight">
-        <span className="text-[#9E9E9E]">{"// Check my github profile"}</span>
-        <span className="text-[20px]">
-          <span className="text-[#4325ED]">const</span> <span className="text-[#75D96C]">github</span> = <a href="https://github.com/machmudeffendi" target="_blank"  role="button" className="text-[#FFAB3E]">'https://github.com/machmudeffendi'</a>
+        <span className="text-[#9E9E9E]">{"// Check my GitHub profile"}</span>
+        <span className="text-lg">
+          <span className="text-[#4325ED]">const</span> <span className="text-[#75D96C]">github</span> ={" "}
+          <a
+            href="https://github.com/machmudeffendi"
+            target="_blank"
+            role="button"
+            className="text-[#FFAB3E] underline"
+          >
+            'https://github.com/machmudeffendi'
+          </a>
         </span>
       </div>
     </div>
-    <div className="flex justify-center ">
+
+    {/* Right Section - Profile Image */}
+    <div className="hidden md:flex justify-center">
       <Image
-        className="dark:invert"
+        className="dark:invert w-48 md:w-96"
         src="/profile.svg"
         alt="Profile Image"
         width={400}
