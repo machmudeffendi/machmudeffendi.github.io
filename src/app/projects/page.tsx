438a033796efe19c1c1d11b0507e44ebf1663c9f
Machmud Effendi <machmud.effendi@syngenta.com>
2025-03-01 13:51:15 +0700

    feat: responsive project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,12 +6,12 @@ import { ProjectType } from "../../components/type/typeProjects";
 export default async function Project(){
   const project = await getAllProject();
   return (
-    <div className="flex flex-col">
-      <h1 className="py-[50px] leading-none text-[40px] font-bold text-white">
+    <div className="flex flex-col lg:px-12">
+      <h1 className="py-[50px] leading-none text-3xl sm:text-4xl md:text-5xl font-bold text-white">
         Workside:<br/>
         The Fenx Projects
       </h1>
-      <div className="grid grid-cols-3 gap-[20px] leading-none">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-[20px] leading-none">
         {project.map((item: ProjectType) => <BoxProjects key={item.slug.current} items={item}/>)}
       </div>
     </div>
