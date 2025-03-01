94162ba5560b2fe6dad287ab5dba493bf1cad70e
Machmud Effendi <machmud.effendi@syngenta.com>
2025-03-01 12:32:51 +0700

    fix: main layout

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -10,8 +10,8 @@ export default function Layout({children}: LayoutType) {
   return (
     <div className="flex flex-col h-screen">
       <Navbar/>
-      <main className="flex-grow border-l border-r border-[#2C334B] p-[20px] pt-[75px]">
-        <div className="container mx-auto h-full px-2 md:px-20">
+      <main className="flex-grow border-l border-r border-[#2C334B] p-4 pt-[75px]">
+        <div className="container mx-auto h-full px-1 md:px-20">
           {children}
         </div>
       </main>
